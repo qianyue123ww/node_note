b906aea25a7053e4de86dcc1be7b54762bb7f82d
dengwenwen <dengwenwen@baidu.com>
2020-08-20 17:10:52 +0800

    2020-8-20

---
+++ node_6.js
@@ -0,0 +1,24 @@
+// Buffer编解码二进制数据包
+
+const buffer1 = Buffer.from('geekbang')
+const buffer2 = Buffer.from([1, 2, 3, 4])
+
+const buffer3 = Buffer.alloc(20)
+
+console.log(buffer1)
+console.log(buffer2)
+console.log(buffer3)
+
+const fs = require('fs')
+const protoBuf = require('protocol-buffers')
+console.log(__dirname)
+const messages = protoBuf(fs.readFileSync(__dirname + '/test.proto'))
+
+const buf = messages.Test.encode({
+    num: 42,
+    payload: 'hello world'
+})
+console.log(buf);
+
+const obj = messages.Test.decode(buf)
+console.log(obj)
--- test1/index.js
+++ test1/index.js
@@ -1,3 +1,10 @@
 // 能取到输入node .js的参数
 console.log(process.argv)
- 
+ var a = require('./lib')
+
+ a.test = 'sb'
+
+ process.stdin.on('data', e => {
+     const t = e.toString().trim()
+     console.log(t)
+ })
